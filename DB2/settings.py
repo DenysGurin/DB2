f58a795bb34b90e0f85399e93c0e5772f7615412
DenysGurin <gurindenys@gmail.com>
2017-07-31 21:16:45 +0300

    Add PostgreSQL

--- DB2/settings.py
+++ DB2/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'DB2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db2',
+        'USER': 'db2user',
+        'PASSWORD': 'db2user123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
